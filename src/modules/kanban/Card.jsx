9a60a18c0be5c9df0596c0b0ddda796cd83ab474
joaoHass <79617643+joaoHass@users.noreply.github.com>
2022-05-02 15:14:37 -0300

    movendo os componentes para o formato de módulos

---
+++ src/modules/kanban/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <>
+      <div class="card">
+        <h3>Exemplo</h3>
+        <p>The cards on the table gin rummy</p>
+      </div>
+    </>
+  )
+}
