badd6bdf32310594ffd40fa71fc59ba0c2972e21
joaoHass <79617643+joaoHass@users.noreply.github.com>
2022-05-02 17:48:27 -0300

    adicionado novas informações na lista de tasks

--- src/modules/kanban/KanbanApp/KanbanApp.jsx
+++ src/modules/kanban/KanbanApp/KanbanApp.jsx
@@ -5,20 +5,28 @@ const TASKS = [
   {
     id: "1",
     name: "Task1",
+    description: "Lorem ipsum",
     category: "Todo",
   },
   {
     id: "2",
     name: "Task2",
+    description: "Tarefa 2 de testes",
     category: "Running",
   },
   {
     id: "3",
     name: "Task3",
+    description: "Mais uma task",
     category: "Revision",
   },
   {
     id: "4",
+    name: "Task4",
+    category: "Concluded",
+  },
+  {
+    id: "5",
     name: "Task4",
     category: "Concluded",
   },
