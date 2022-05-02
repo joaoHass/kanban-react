c0fd803e22d19e25bbf2ac709aa18587f2228e3a
joaoHass <79617643+joaoHass@users.noreply.github.com>
2022-05-02 17:03:35 -0300

    Apagado código desnecessário; refatorado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { useState } from "react"
 import "./App.css"
-import Card from "@modules/kanban/Card"
-
-function ColumnStack() {}
-
-function KanbanApp() {}
+import KanbanApp from "./modules/kanban/KanbanApp/KanbanApp"
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <button onClick={() => setCount(count + 1)}>
-      Foi clicado {count} {count === 1 ? "vez" : "vezes"}
-    </button>
+    <>
+      <KanbanApp></KanbanApp>
+    </>
   )
 }
