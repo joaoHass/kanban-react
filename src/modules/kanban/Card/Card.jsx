7e7ad0a56726cadf23e53cc1c217dde9fdfd260b
joaoHass <joaopedrohass@outlook.com>
2022-05-04 16:29:03 -0300

    added new props to implement delete func

--- src/modules/kanban/Card/Card.jsx
+++ src/modules/kanban/Card/Card.jsx
@@ -1,12 +1,19 @@
 import "./Card.css"
 
-export default function Card({ name = "", description = "", category }) {
+export default function Card({
+  id,
+  name = "",
+  description = "",
+  category,
+  deleteTask,
+}) {
   return (
     <>
       <div className="card">
         <h3>{name}</h3>
         <p>{description}</p>
         <p className="status">{category}</p>
+        <button onClick={() => deleteTask(id)}>Deletar</button>
       </div>
     </>
   )
