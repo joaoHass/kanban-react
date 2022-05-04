02f0bd8f8d96dfb157a70eedad1f0c8c746a3181
joaoHass <joaopedrohass@outlook.com>
2022-05-04 16:29:22 -0300

    passing new props to implement delete func

--- src/modules/kanban/ColumnStack/ColumnStack.jsx
+++ src/modules/kanban/ColumnStack/ColumnStack.jsx
@@ -1,16 +1,18 @@
 import Card from "../Card/Card"
 import "./ColumnStack.css"
 
-export default function ColumnStack({ tasks }) {
+export default function ColumnStack({ tasks, deleteTask }) {
   return (
     <>
       <div className="stack">
         {tasks.map((task) => (
           <Card
             key={task.id}
+            id={task.id}
             name={task.name}
             description={task.description}
             category={task.category}
+            deleteTask={deleteTask}
           ></Card>
         ))}
       </div>
