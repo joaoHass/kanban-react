b89f152418451230e4d2cc7f906f9f59ee6f3e80
joaoHass <joaopedrohass@outlook.com>
2022-05-06 13:42:52 -0300

    implementing drag-n-drop functionality

--- src/modules/kanban/Card/Card.jsx
+++ src/modules/kanban/Card/Card.jsx
@@ -6,10 +6,33 @@ export default function Card({
   description = "",
   category,
   deleteTask,
+  changeTaskStatus,
 }) {
+  function onDragStart(e) {
+    setTimeout(() => {
+      e.target.classList.add("hide")
+    }, 0)
+  }
+
+  function onDragEnd(e) {
+    // verificando se o pai é uma stack válida
+    if (e.target.parentNode.classList.contains("stack")) {
+      // mudar isso
+      // estou pegando o id do stack pai atual, preciso pegar do stack alvo
+      // implementar isso no drop da ColumnStack
+      changeTaskStatus(id, e.target.parentNode.attributes["id"])
+    }
+    e.target.classList.remove("hide")
+  }
+
   return (
     <>
-      <div className="card">
+      <div
+        className="card"
+        draggable="true"
+        onDragStart={(e) => onDragStart(e)}
+        onDragEnd={(e) => onDragEnd(e)}
+      >
         <h3>{name}</h3>
         <p>{description}</p>
         <p className="status">{category}</p>
