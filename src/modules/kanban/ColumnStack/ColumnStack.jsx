0b2ebae17fc3e3f47d76f42748a03c5b81b080cc
joaoHass <joaopedrohass@outlook.com>
2022-05-06 13:43:37 -0300

    implementing drag-n-drop functionality

--- src/modules/kanban/ColumnStack/ColumnStack.jsx
+++ src/modules/kanban/ColumnStack/ColumnStack.jsx
@@ -1,23 +1,58 @@
 import Card from "../Card/Card"
 import "./ColumnStack.css"
 
-export default function ColumnStack({ title, tasks, deleteTask }) {
+export default function ColumnStack({
+  title,
+  id,
+  tasks,
+  deleteTask,
+  changeTaskStatus,
+}) {
+  function dragEnter(e) {
+    // deixando a stack um objeto válido para drops
+    e.preventDefault()
+    e.target.classList.add("drag-over")
+  }
+
+  function dragOver(e) {
+    e.preventDefault()
+    e.target.classList.add("drag-over")
+  }
+
+  function dragLeave(e) {
+    e.target.classList.remove("drag-over")
+  }
+
+  function drop(e) {
+    e.target.classList.remove("drag-over")
+  }
+
   return (
     <>
-      <div className="stack">
+      <div className="wrapper">
         <h2>
           {title} ({tasks.length})
         </h2>
-        {tasks.map((task) => (
-          <Card
-            key={task.id}
-            id={task.id}
-            name={task.name}
-            description={task.description}
-            category={task.category}
-            deleteTask={deleteTask}
-          ></Card>
-        ))}
+        <div
+          className="stack"
+          id={id}
+          onDragEnter={(e) => dragEnter(e)}
+          onDragOver={(e) => dragOver(e)}
+          onDragLeave={(e) => dragLeave(e)}
+          onDrop={(e) => drop(e)}
+        >
+          {tasks.map((task) => (
+            <Card
+              key={task.id}
+              id={task.id}
+              name={task.name}
+              description={task.description}
+              category={task.category}
+              deleteTask={deleteTask}
+              changeTaskStatus={changeTaskStatus}
+            ></Card>
+          ))}
+        </div>
       </div>
     </>
   )
--- src/modules/kanban/KanbanApp/KanbanApp.jsx
+++ src/modules/kanban/KanbanApp/KanbanApp.jsx
@@ -8,7 +8,6 @@ const TASKS = JSON.parse(window.localStorage.getItem("tasks")) || []
 
 export default function KanbanApp() {
   const [tasks, setTasks] = useState(TASKS)
-
   function createNewTask(name = "", desc = "") {
     setTasks([
       ...tasks,
@@ -29,31 +28,92 @@ export default function KanbanApp() {
     )
   }
 
+  function changeTaskStatus(taskId, category) {
+    // pegando o index da task que precisamos alterar o estado
+    let categoryy = "Todo"
+    if (category === 2) categoryy = "Running"
+    if (category === 2) categoryy = "Revision"
+    if (category === 2) categoryy = "Concluded"
+    console.log(categoryy)
+    const taskToChangeId = tasks.findIndex((x) => x.id === taskId)
+    // alterando ele
+    tasks[taskToChangeId] = {
+      ...tasks[taskToChangeId],
+      category: categoryy,
+    }
+
+    console.log(Array.isArray(tasks), tasks)
+  }
+
   window.localStorage.setItem("tasks", JSON.stringify(tasks))
+  console.log(
+    tasks,
+    Array.isArray(tasks),
+    Array.isArray(JSON.parse(window.localStorage.getItem("tasks")))
+  )
+
+  const STACKS = [
+    {
+      name: "Todo",
+      id: 1,
+    },
+    {
+      name: "In Progress",
+      id: 2,
+    },
+    {
+      name: "In Revision",
+      id: 3,
+    },
+    {
+      name: "Concluded",
+      id: 4,
+    },
+  ]
 
   return (
     <>
       <TaskCreator createNewTask={createNewTask}></TaskCreator>
       <div className="kanban">
+        {/*         {STACKS.forEach((column) => {
+          ;<ColumnStack
+            title={column.name}
+            tasks={tasks.filter((task) => task.category === column.name)}
+            deleteTask={deleteTask}
+            changeTaskStatus={changeTaskStatus}
+          ></ColumnStack>
+        })} */}
         <ColumnStack
           title={"Todo"}
-          tasks={tasks.filter((task) => task.category === "Todo")}
+          id="1"
+          tasks={
+            Array.isArray(tasks)
+              ? tasks.filter((task) => task.category === "Todo")
+              : console.log("n")
+          }
           deleteTask={deleteTask}
+          changeTaskStatus={changeTaskStatus}
         ></ColumnStack>
         <ColumnStack
           title={"Running"}
+          id="2"
           tasks={tasks.filter((task) => task.category === "Running")}
           deleteTask={deleteTask}
+          changeTaskStatus={changeTaskStatus}
         ></ColumnStack>
         <ColumnStack
           title={"In Revision"}
+          id="3"
           tasks={tasks.filter((task) => task.category === "Revision")}
           deleteTask={deleteTask}
+          changeTaskStatus={changeTaskStatus}
         ></ColumnStack>
         <ColumnStack
           title={"Concluded"}
+          id="4"
           tasks={tasks.filter((task) => task.category === "Concluded")}
           deleteTask={deleteTask}
+          changeTaskStatus={changeTaskStatus}
         ></ColumnStack>
       </div>
     </>
